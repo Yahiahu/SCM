18c715df1459136c256cb3e14a2146b01121e720
Yahia Hussein <yahiahu24@gmail.com>
2025-06-19 09:44:46 -0400

    createaccoutn page vercel

--- scm-platform/frontend/src/pages/createAccount.tsx
+++ scm-platform/frontend/src/pages/createAccount.tsx
@@ -171,7 +171,7 @@ export default function CreateAccount() {
     }
 
     try {
-      const res = await fetch("http://localhost:5001/api/users", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
